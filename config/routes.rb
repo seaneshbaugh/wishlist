fd4c3cd089fa6623736ff284107ce86c5d4f6b56
Sean Eshbaugh <seaneshbaugh@gmail.com>
2026-07-31 14:47:09 -0500

    Added User model.

--- config/routes.rb
+++ config/routes.rb
@@ -12,26 +12,26 @@ Rails.application.routes.draw do
   devise_for :users, skip: %i[sessions passwords registrations confirmations unlocks]
 
   devise_scope :user do
-    get '/login' => 'devise/sessions#new', as: :new_user_session
-    post '/login' => 'devise/sessions#create', as: :user_session
-    delete '/logout' => 'devise/sessions#destroy', as: :destroy_user_session
+    get "/login" => "devise/sessions#new", as: :new_user_session
+    post "/login" => "devise/sessions#create", as: :user_session
+    delete "/logout" => "devise/sessions#destroy", as: :destroy_user_session
 
-    post '/update-password' => 'devise/passwords#create', as: :user_password
-    get '/reset-password' => 'devise/passwords#new', as: :new_user_password
-    get '/update-password' => 'devise/passwords#edit', as: :edit_user_password
-    put '/update-password' => 'devise/passwords#update'
+    post "/update-password" => "devise/passwords#create", as: :user_password
+    get "/reset-password" => "devise/passwords#new", as: :new_user_password
+    get "/update-password" => "devise/passwords#edit", as: :edit_user_password
+    put "/update-password" => "devise/passwords#update"
 
-    post '/register' => 'registrations#create', as: :user_registration
-    get '/register' => 'devise/registrations#new', as: :new_user_registration
+    post "/register" => "registrations#create", as: :user_registration
+    get "/register" => "devise/registrations#new", as: :new_user_registration
 
-    post '/confirm-registration' => 'devise/confirmations#create', as: :user_confirmation
-    get '/resend-confirmation' => 'devise/confirmations#new', as: :new_user_confirmation
-    get '/confirm-registration' => 'confirmations#show'
+    post "/confirm-registration" => "devise/confirmations#create", as: :user_confirmation
+    get "/resend-confirmation" => "devise/confirmations#new", as: :new_user_confirmation
+    get "/confirm-registration" => "confirmations#show"
 
-    post '/unlock-account' => 'devise/unlocks#create', as: :user_unlock
-    get '/resend-unlock' => 'devise/unlocks#new', as: :new_user_unlock
-    get '/unlock-account' => 'devise/unlocks#show'
+    post "/unlock-account" => "devise/unlocks#create", as: :user_unlock
+    get "/resend-unlock" => "devise/unlocks#new", as: :new_user_unlock
+    get "/unlock-account" => "devise/unlocks#show"
   end
 
-  root to: 'devise/sessions#new'
+  root to: "devise/sessions#new"
 end
