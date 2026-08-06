259ab4a42a03c748e24dffeeb5b92f9faaa5b610
Sean Eshbaugh <seaneshbaugh@gmail.com>
2026-08-06 12:48:08 -0500

    Added layout.

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,3 +7,16 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+
+document.addEventListener("turbo:load", () => {
+  const button = document.getElementById("mobile-menu-button");
+  const menu = document.getElementById("mobile-menu");
+
+  if (!button || !menu) {
+    return;
+  }
+
+  button.addEventListener("click", () => {
+    menu.classList.toggle("hidden");
+  });
+});
